2c32cb13da43f56289d4e1aca6eea10072b22609
guochao.song <guochao.song@dmall.com>
2020-05-05 21:07:07 +0800

    temp save

--- src/compoents/Routers.jsx
+++ src/compoents/Routers.jsx
@@ -1,26 +1,27 @@
-// /**
-//  * Created by senro on 17/3/18.
-//  */
-// import React from 'react';
-// import {Router, Route,hashHistory,IndexRoute } from 'react-router';
-//
-//
-//
-// //{{mainLayout.import}}
-//
-// export default class extends React.Component {
-//     render() {
-//         return (
-//             <Router history={ hashHistory }>
-//                 <Route path="/pageEditor" component={PageEditor}/>
-//
-//                 <Route path="systemLayoutNoBreadcrumb" component={SystemLayoutNoBreadcrumb}>
-//                     <Route path="/eventslist" component={Eventslist}/>
-//                     <Route path="/eventslistDetail" component={EventslistDetail}/>
-//                 </Route>
-//
-//             </Router>
-//         )
-//
-//     }
-// }
+/**
+ * Created by senro on 17/3/18.
+ */
+import React from 'react';
+import {Router, Route,hashHistory,IndexRoute } from 'react-router';
+
+import Page1 from 'page1/page1'
+
+
+//{{mainLayout.import}}
+
+export default class extends React.Component {
+    render() {
+        return (
+            <Router history={ hashHistory }>
+                <Route path="/Page1" component={Page1}/>
+
+                {/*<Route path="systemLayoutNoBreadcrumb" component={SystemLayoutNoBreadcrumb}>*/}
+                {/*    <Route path="/eventslist" component={Eventslist}/>*/}
+                {/*    <Route path="/eventslistDetail" component={EventslistDetail}/>*/}
+                {/*</Route>*/}
+
+            </Router>
+        )
+
+    }
+}
